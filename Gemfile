0647446947100324674160a423e8f74b535431a9
Matt Nguyen <mnguyen916@gmail.com>
2012-11-17 03:31:51 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'jquery-rails'
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liveplaylist::Application.config.secret_token = 'a7eab3ba83589cb5ab78e54c68048cadf0ec01a42cf846eaff1e88c37db2f89a52a4e1c671f15881778eea06b55446f64677d45f2a2755d6841920c30aa4479f'
