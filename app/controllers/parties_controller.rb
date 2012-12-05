d4159037a07d142a790a67f1482c7100198cb190
Basil Siddiqui <basil.siddiqui@gmail.com>
2012-12-04 19:53:48 -0500

    Worked on search

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,4 +1,7 @@
 class PartiesController < ApplicationController
 	def index
 	end
+
+	def create
+	end
 end
--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,8 @@ module Liveplaylist
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.paths += %w( static )
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
